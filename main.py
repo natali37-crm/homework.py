d9b8c16e1dcefd6c9f7e39d94689da3cd07cc5e6
natali37-crm <natal.filimonova2016@yandex.ru>
2024-06-27 20:51:56 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+number=('всего задач 12')
+print(number)
+time=('затрачено времени 1.5')
+print(time)
+lesson=('курс Python')
+print(lesson)
+print(1.5/12)
+averge=('среднее время 0.125')
+print(averge)
+
+print(lesson,number,time,averge)
+
+first_name='Vika'
+flower_count=5
+second_name='Dasha'
+postcard_count=1
+print(first_name,'дала', second_name, flower_count,'цветов и', postcard_count,'открытку')
+
+
+
+
+
+
